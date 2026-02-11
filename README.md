afba1ef09254989c0c0f2a1b375644d5d549dfd7
Alexander <alexander@makklays.com.ua>
2026-02-11 21:22:58 +0100

    11-02-2026 Add new README and 'Hello, Kotlin'

--- README.md
+++ README.md
@@ -2,3 +2,28 @@
 
 This is a project in Kotlin with Spring Boot, etc. 
 
+I want to demonstrate in this project my knowledge, such as:
+- REST API
+- CRUD
+- DTO
+- Mapper
+- Validation
+- Exception handling
+- PostgreSQL
+- Flyway
+- Docker
+- Kubernetes
+- JWT
+- Clean / Hexagonal Architecture
+
+#### And next tech stack:
+- Kotlin
+- Spring Boot
+- Spring Web
+- Spring Data JPA
+- PostgreSQL
+- Flyway
+- Docker
+- Testcontainers
+- JUnit 5
+
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,17 @@
+/**
+ * Kotlin is a modern programming language that makes developers happier.
+ * It’s concise, safe, interoperable and tool-friendly.
+ *
+ * @author Alexander Kuziv <makklays@gmail.com>
+ * @company TechMatrix18
+ * @since 11.02.2026
+ * @version 0.0.1
+ */
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("Hello, Kotlin!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 }
+
